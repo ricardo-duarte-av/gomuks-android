f849dd62295cdf16c1ea49c06fce5bdb95c624d1
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2025-03-20 21:45:26 +0000

    Update MainActivity.kt

--- app/src/main/java/app/gomuks/android/MainActivity.kt
+++ app/src/main/java/app/gomuks/android/MainActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
